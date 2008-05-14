c0f47f58415fc137a8b52be1e63c2f7478cbcc34
8motions <8motions@c04499f2-5e49-0410-9849-ada19a1a0ed9>
2008-05-14 09:16:41 +0000

    Add another client in description

--- trunk/app/models/cell.rb
+++ trunk/app/models/cell.rb
@@ -13,6 +13,27 @@ class Cell < ActiveRecord::Base
     self.save
   end
   
+  def Cell.computeAverage cells
+    cell=Cell.new
+    if cells.size>0
+      cells.each do |c|
+        cell.lat=cell.lat+c.lat
+        cell.lon=cell.lon+c.lon
+      end
+    
+      cell.lat=cell.lat/cells.size
+      cell.lon=cell.lon/cells.size
+      c=cells.first
+      cell.mcc=c.mcc
+      cell.mnc=c.mnc
+      cell.lac=c.lac
+      cell.range=1000  # need to be computed here!
+      cell.updated_at=Time.now
+      cell.nbSamples=cells.size
+    end
+    cell
+  end
+  
   def operator
     Operators.find_by_mcc_and_mnc(self.mcc,self.mnc)
   end
